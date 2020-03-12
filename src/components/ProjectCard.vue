8496cdb811fa1bb1ff7f8c305dd5cea766ca6879
benja <benjamin.hosenfeld@web.de>
2020-03-12 22:20:32 +0100

    eslint init, needs to be configured. empty project component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="project-card">
+    <div class="header">
+      {{ project.name }}
+      <div class="actions"></div>
+    </div>
+    <div class="content">
+      {{ project.description }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => {
+    return {
+
+    }
+  },
+  beforeMount () {
+    console.log('project: ', this.project)
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.project-card
+  @apply 
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Here should be my projects, refer to Github</div>
+  <div>
+    <div>Here should be my projects, refer to Github</div>
+    <project-card
+      v-for="project in projects"
+      :key="project.id"
+      :project="project"
+    />
+  </div>
 </template>
 
 <script>
+import ProjectCard from '@/components/ProjectCard'
+
 export default {
-  
+  components: { ProjectCard },
+  data: () => {
+    return {
+      projects: [
+        { id: 0, name: 'first project' , desciption: 'Nothing has been done here yet' },
+        { id: 1, name: 'second project' , desciption: 'Nothing has been done here yet' }
+      ]
+    }
+  }
 }
 </script>
